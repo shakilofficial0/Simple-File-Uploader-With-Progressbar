57698ad3601e015812484cb537253e4f9e1f9377
Md. Saifur Rahman <34965777+mdsaifurrahmann@users.noreply.github.com>
2021-05-31 00:47:17 +0600

    dependency updated
    Auto folder creation feature added

--- dependency.php
+++ dependency.php
@@ -5,7 +5,8 @@
       $file_size =$_FILES['file']['size'];
       $file_tmp =$_FILES['file']['tmp_name'];
       $file_type=$_FILES['file']['type'];
-      $path = "uploads/".$file_name;
+      $file_path = "uploads/".$file_name;
+      $dir_path = "uploads";
    
       
       $extensions= array("html","php","js");
@@ -16,12 +17,19 @@
       }
       
       if(empty($errors)==true){
-         if(!file_exists($path)){
-            move_uploaded_file($file_tmp,"uploads/".$file_name);
-            echo "File Uploaded <a href='uploads/$file_name'>Here</a>";
-         }
-         else{
-            echo "This file is already uploaded!";
+         if(!file_exists($dir_path)){
+            if(!mkdir($dir_path, 0777, true)){
+               die('Failed to create Folder \' Uploads\' ');
+            }
+            else{
+               if(!file_exists($file_path)){
+                  move_uploaded_file($file_tmp,"uploads/".$file_name);
+                  echo "File Uploaded <a href='uploads/$file_name'>Here</a>";
+               }
+               else{
+                  echo "This file is already uploaded!";
+               }
+            }
          }
       }else{
          print_r($errors);
